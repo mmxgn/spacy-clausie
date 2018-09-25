4a469b29da13057e0e4096fa62e31eeb9e2724d8
Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
2018-09-25 18:44:16 +0100

    initial code commit

---
+++ clausiepy/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 17:57:27 2018
+
+@author: Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
+
+Reimplementation in spacy+python of:
+    
+Del Corro Luciano, and Rainer Gemulla. 
+"Clausie: clause-based open information extraction." 
+Proceedings of the 22nd international conference on World Wide Web. ACM, 2013.
+
+"""
+
+from .clausiepy import *
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 18:21:51 2018
+
+@author: Emmanouil Theofanis Chourdakis
+"""
+
+
+
+from setuptools import setup, find_packages
+setup(
+    name="clausiepy",
+    version="0.0.1",
+    packages=find_packages(),
+   # scripts=['clausie/clausie.py', 'clausie/__init__.py'],
+    install_requires=['spacy>=2.0.0'],
+    package_data={
+            'clausiepy': ['*.txt']
+    },
+    
+    author="Emmanouil Theofanis Chourdakis",
+    author_email="e.t.chourdakis@qmul.ac.uk", 
+    description="A reimplementation of ClausIE Information Extraction System in python",
+    url="https://github.com/mmxgn/clausiepy",
+    keywords="openie clausie information extraction",
+    include_package_data=True,
+    
+)
