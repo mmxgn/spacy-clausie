79b5fe3ce0bd9533df921a68cbf1c39728ea53b2
Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
2018-09-26 15:07:31 +0100

    Added a cooler example

--- problog/test_clausie.pl
+++ problog/test_clausie.pl
@@ -1,5 +1,5 @@
 :-use_module('clausiepy_pl.py').
 
-query(clausie('Albert Einstein died in Princeton in 1955.', Subject, Verb, IndirectObject, DirectObject, Complement, Adverb)).
+query(clausie('Albert Einstein, a scientist of the 20th century, died in Princeton in 1955.', Subject, Verb, IndirectObject, DirectObject, Complement, Adverb)).
 
 
