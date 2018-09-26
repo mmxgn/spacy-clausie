c0e71b857d8444a383c8c3d5f32f1e8a905b8205
Emmanouil Theofanis Chourdakis <e.t.chourdakis@qmul.ac.uk>
2018-09-26 14:38:31 +0100

    Added bindings for problog and test file

---
+++ problog/clausiepy_pl.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 13:38:38 2018
+
+@author: Emmanouil Theofanis Chourdakis
+
+Problog module for extracting information from a sentence using clausiepy
+
+"""
+
+from problog.extern import problog_export_nondet
+
+import clausiepy as cl
+
+def remove_apostrophe(string):
+    # Remove "'"S
+    if string[0] == "'":
+        string = string[1:]
+    if string[-1] == "'":
+        string = string[:-1]    
+        
+    return string
+
+@problog_export_nondet('+str', '-str', '-str', '-str', '-str', '-str', '-str', '-str')
+def clausie(sent):
+    
+    sent = remove_apostrophe(sent)
+    
+    clauses = cl.clausie(sent)
+    
+    propositions = cl.extract_propositions(clauses)    
+
+    result = []
+    for proposition in propositions:
+        ptext = cl.proposition_text(proposition)
+        result.append(ptext)
+        
+    return result
---
+++ problog/test_clausie.pl
@@ -0,0 +1,5 @@
+:-use_module('clausiepy_pl.py').
+
+query(clausie('Albert Einstein died in Princeton in 1955.', X1, X2, X3, X4, X5, X6, X7)).
+
+
